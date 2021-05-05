7dd6bb0bacca70e1a1ed35f1a16c20ef6107f9e3
Yefri <u201912400@upc.edu.pe>
2021-05-05 16:07:56 -0500

    Descripción de las variables

---
+++ Scripts/script1.R
@@ -0,0 +1,10 @@
+data <- read.csv("C:/Users/PC/Desktop/GitHub/TA1_Administracion/data/hotel_bookings.csv",
+                 header=TRUE, sep=",")
+
+head(data)
+
+names(data)
+
+str(data)
+
+summary(data)
